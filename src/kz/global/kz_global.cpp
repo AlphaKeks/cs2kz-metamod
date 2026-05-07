d8f28acc93f45eea45277c316500ef88af77dd0d
zer0.k <zer0kams@gmail.com>
2026-05-07 06:37:12 +0200

    Add global replay upload support

--- src/kz/global/kz_global.cpp
+++ src/kz/global/kz_global.cpp
@@ -356,6 +356,8 @@ void KZGlobalService::OnServerGamePostSimulate()
 					KZGlobalService::ws.messageCallbacks.callbacks.merge(messageCallbacks);
 				}
 			}
+
+			KZGlobalService::replayManager.ProcessUploads();
 		}
 		break;
 
